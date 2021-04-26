1cba6ebebb126d3b5327333a0806024adf08694c
Marc <marc.marechal@dell.com>
2021-04-26 14:06:12 +0800

    Message Converter Configuration

---
+++ src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by marecm on 4/26/2021
+ */
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}

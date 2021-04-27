fa538c13384b1a0d97ef7c61272a3129bfc061b8
Marc <marc.marechal@dell.com>
2021-04-27 14:35:04 +0800

    Using Local ActiveMQ Broker

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -1,9 +1,10 @@
 package guru.springframework.sfgjms;
 
 
-import org.apache.activemq.artemis.core.config.Configuration;
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.embedded.EmbeddedActiveMQ;
+//import org.apache.activemq.artemis.core.config.Configuration;
+//import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+//import org.apache.activemq.artemis.core.server.embedded.EmbeddedActiveMQ;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,16 +13,16 @@ public class SfgJmsApplication {
 
     public static void main(String[] args) throws Exception {
 
-        Configuration config = new ConfigurationImpl();
-        config.addAcceptorConfiguration("in-vm", "vm://0");
-        config.setPersistenceEnabled(false);
-        config.setJournalDirectory("target/data/journal");
-        config.setSecurityEnabled(false);
-
-        EmbeddedActiveMQ server = new EmbeddedActiveMQ();
-        server.setConfiguration(config);
-
-        server.start();
+//        Configuration config = new ConfigurationImpl();
+//        config.addAcceptorConfiguration("in-vm", "vm://0");
+//        config.setPersistenceEnabled(false);
+//        config.setJournalDirectory("target/data/journal");
+//        config.setSecurityEnabled(false);
+//
+//        EmbeddedActiveMQ server = new EmbeddedActiveMQ();
+//        server.setConfiguration(config);
+//
+//        server.start();
 
         SpringApplication.run(SfgJmsApplication.class, args);
     }

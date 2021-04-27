ca3020d02e3bda7a2cbf43c87f33ffea7e3e29c6
Marc <marc.marechal@dell.com>
2021-04-27 09:59:14 +0800

    Receive JMS Messages

---
+++ src/main/java/guru/springframework/sfgjms/listener/HelloWorldListener.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgjms.listener;
+
+import guru.springframework.sfgjms.config.JmsConfig;
+import guru.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+/**
+ * Created by marecm on 4/27/2021
+ */
+@Slf4j
+@Component
+public class HelloWorldListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers,
+                       Message message) {
+        log.info("I got a message!!!");
+        log.info(helloWorldMessage.toString());
+    }
+}
